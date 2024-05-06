f870f2d4dd5d072cee40403b5eaebc8b469d21fb
wogks <wogks27@naver.com>
2024-05-06 22:29:01 +0900

    method

--- app/src/main/java/com/example/myapplication/main.kt
+++ app/src/main/java/com/example/myapplication/main.kt
@@ -1,5 +1,15 @@
 package com.example.myapplication
 
 fun main(){
-    print("hello")
+    var x = 10
+    var str = "Hello"
+    var isMarried = true
+
+    //상수
+    val a = 0
+
+}
+
+fun myMethod(a:Int,b:Int):Int{
+    return a + b
 }
